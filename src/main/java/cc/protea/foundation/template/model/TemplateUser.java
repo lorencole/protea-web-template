0633cbb42729123d478a7c719cbad31309c478a4
Loren Cole <loren@protea.cc>
2016-05-11 10:34:06 -0500

    Added sql for template_user table, added TemplateUser.update() method

--- src/main/java/cc/protea/foundation/template/model/TemplateUser.java
+++ src/main/java/cc/protea/foundation/template/model/TemplateUser.java
@@ -1,8 +1,41 @@
 package cc.protea.foundation.template.model;
 
+import java.sql.ResultSet;
+
+import org.skife.jdbi.v2.Handle;
+
+import cc.protea.foundation.integrations.DatabaseUtil;
+import cc.protea.foundation.integrations.JsonUtil;
 import cc.protea.foundation.utility.ProteaUser;
 
 public class TemplateUser extends ProteaUser {
 
 	public String profilePictureUrl;
+	
+	public String getTableName() {
+		return "template_user";
+	}
+
+	@Override
+	public UserMapper<TemplateUser> mapper() {
+		UserMapper<TemplateUser> mapper = new ProteaUser.UserMapper<TemplateUser>() {
+			@Override
+			public TemplateUser extend(TemplateUser u, final ResultSet rs) {
+				u.profilePictureUrl = DatabaseUtil.getString(rs, "profilePictureUrl");
+				return u;
+			}
+		};
+		return mapper;
+	}
+	
+	@Override
+	public void update(Handle h, String additionalFields) {
+		TemplateUser template = JsonUtil.fromJson(additionalFields, TemplateUser.class);
+		h.createStatement("UPDATE " + getTableName() + " SET "
+				+ " profile_picture_url = :profilePictureUrl"
+				+ " WHERE user_key = :userKey")
+			.bind("profilePictureUrl", template.profilePictureUrl)
+			.bind("userKey", this.id)
+			.execute();
+	}
 }
---
+++ src/main/resources/sql/initial-mysql.sql
@@ -0,0 +1,5 @@
+create table template_user (
+	user_key INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    profile_picture_url VARCHAR(255)
+) ENGINE = innodb CHARACTER SET utf8 COLLATE utf8_general_ci;
+
