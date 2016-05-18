a2648f6b2b857e070c741be2b3dc28d7e9139b16
Loren Cole <loren@protea.cc>
2016-05-18 11:12:51 -0500

    Added public endpoint for swagger

--- src/main/java/cc/protea/foundation/example/Main.java
+++ src/main/java/cc/protea/foundation/example/Main.java
@@ -10,7 +10,8 @@ public class Main {
 	public static void main(final String[] args) throws Exception {
 
 		ProfoundConfiguration.systemEmails.defaultTemplate = "platform-email";
-		ProfoundConfiguration.publicUrl = "http://localhost:8080";
+		//ProfoundConfiguration.publicUrl = "http://localhost:8080";
+		ProfoundConfiguration.publicUrl = "http://protea-web-template.herokuapp.com/";
 		ProfoundConfiguration.servicePackages.add("cc.protea.foundation.template.services");
 		ProfoundConfiguration.createSocialAccounts = true;
 
