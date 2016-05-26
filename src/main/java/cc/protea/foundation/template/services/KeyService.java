2d02ac349c72d533acbc991d4ef6e9c8bfb650e0
Loren Cole <loren@protea.cc>
2016-05-26 13:06:03 -0500

    Added KeyService becuase it's super useful for debugging

---
+++ src/main/java/cc/protea/foundation/template/services/KeyService.java
@@ -0,0 +1,34 @@
+package cc.protea.foundation.template.services;
+
+import javax.annotation.security.PermitAll;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.lang3.StringUtils;
+
+import cc.protea.foundation.util.KeyUtil;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+
+@Path("/key")
+@Api(value = "/key", description = "Key")
+@Produces(MediaType.TEXT_PLAIN)
+public class KeyService {
+
+	@GET
+	@Path("/{id}")
+	@PermitAll
+	@ApiOperation(value = "Translate a key", response = String.class)
+	public String transalate(@PathParam("id") final String id) {
+		if (StringUtils.isNumeric(id)) {
+			return KeyUtil.toString(Long.valueOf(id));
+		}
+		Long key = KeyUtil.toKey(id);
+		return key == null ? null : key.toString();
+	}
+
+}
