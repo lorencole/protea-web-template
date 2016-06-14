d173945835d905083052d9f6d6c69d9239b5595f
Loren Cole <loren@protea.cc>
2016-06-14 14:46:14 -0500

    Removed knockout and freemarker

--- src/main/freemarker/lib/common.ftl
+++
@@ -1,5 +0,0 @@
-<#include "internal-page.ftl.html">
-<#if pp.sourceDirectory == 'authenticated/' || pp.sourceDirectory == 'unknown/'>
-<#include "../theme/${pp.sourceDirectory}${pp.sourceDirectory?remove_ending('/')}-lib.html" ignore_missing=true>
-<#include "../theme/${pp.sourceDirectory}${pp.sourceDirectory?remove_ending('/')}-page.html">
-</#if>
--- src/main/freemarker/templates/configuration.js
+++
@@ -1,8 +0,0 @@
-// Set these to enable features
-
-Profound.Config.Social = {};
-
-Profound.Config.Social.facebook = '1525659471006265';
-Profound.Config.Social.google   = '129606498407-7dkt6329l3sgfgjck09rn8avj042hut1.apps.googleusercontent.com';
-Profound.Config.Social.twitter  = 'maUVScpJ3ndp817EQNdCB9d0F';
-Profound.Config.Social.linkedin = '75rpy5gpux9oxm';
